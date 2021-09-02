466b85876e346c7fc575feffae16d17d9fbeb67b
Chansung Park <deep.diver.csp@gmail.com>
2021-09-02 13:14:13 +0900

    Update google_codelab_step_overide.js

--- static/google_codelab_step_overide.js
+++ static/google_codelab_step_overide.js
@@ -1,3 +1,9 @@
+var topElement = document.getElementsByTagName("google-codelab")[0];
+var title = topElement.getAttribute("codelab-title");
+var authors = topElement.getAttribute("authors");
+console.log(title);
+console.log(authors);
+
 var steps = document.getElementsByTagName("google-codelab-step");
 
 var innerBeforeIntruction = document.querySelectorAll(".instructions")[0];
@@ -9,21 +15,15 @@ innerNewInstruction.className = "instructions about-card";
 var innerTitle = document.createElement("div");
 innerTitle.className = "inner inner-title";
 innerTitle.innerHTML = `
-        <h1>Introduction to TFX CLI</h1>
+        <h1>${title}</h1>
         `;
 innerNewInstruction.append(innerTitle);
 
 var innerAuthor = document.createElement("div");
 innerAuthor.className = "inner inner-author";
 innerAuthor.innerHTML = `
-        <span>by Chansung Park</span>
+        <span>by ${authors}</span>
         `;
 innerNewInstruction.append(innerAuthor);
 
-var innerHorizontalLine = document.createElement("div");
-innerHorizontalLine.innerHTML = `
-        <hr/>
-        `;
-
 steps[0].insertBefore(innerNewInstruction, innerBeforeIntruction);
-steps[0].insertBefore(innerBeforeIntruction, innerHorizontalLine);
